08c7255808c2ea7efdd4dd6a1800071296314fd9
yaman karna <yaman.karna@gmail.com>
2022-06-15 14:38:58 -0500

    Revert "intilization"
    This reverts commit e9ed62dc492b753dd8f4a78297abc56a4bde9728.

--- src/main/java/com/zorba/shoppingCart/repository/ShoppingCartRepository.java
+++
@@ -1,6 +0,0 @@
-package com.zorba.shoppingCart.repository;
-import org.springframework.data.repository.CrudRepository;
-
-import com.zorba.shoppingCart.entity.Customer;
-public interface ShoppingCartRepository extends CrudRepository<Customer, Long>{
-}
